f897e45cae7222998818741413e8872e7bba1cdb
Robertas <rugius@gmail.com>
2013-11-19 16:02:46 +0200

    Mazuju sriciu random searches:

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <fstream>
 
-double xx[100][2];
+double xx[100][2], yy[20][2];
 
 template <class T>
   void exch(T &A, T &B)
@@ -49,30 +49,26 @@ int main(int argc, char *argv[])
 
     double f,f2,min=0;
     int v1, v2;
-   srand(100);   //srand (time(NULL));
+   srand(101);   //srand (time(NULL));
    ofstream myfile, myfile2, myfile3;
    myfile.open ("rezvisixf.txt");
-
+double a1 = -5 ,a2 = 10 ,a3 = 0 ,a4 = 15;
 
   for(int i=0;i<100;i++){
-  x[0] = frand(-5, 10);
+  x[0] = frand(a1, a2);
 xx[i][0] = x[0];
-  x[1] = frand(0, 15);
+  x[1] = frand(a3, a4);
 xx[i][1] = x[1];
   f = branin(&x[0]);
 
 if (myfile.is_open())
  myfile <<  x[0] << " " << x[1] << " " << f << "\n";
 
-
-
     cout << i+1 << " : " << f << " taskai : " << x[0] << " " <<  x[1] << endl;
    if (i==0) min = f;
    else if (min > f) {min = f; xMin[0] = x[0]; xMin[1] = x[1]; }; w[i]=f;
 
   }
-   cout << "Minimumas : " << min << endl;
-   cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;
 
    myfile.close();
 
@@ -95,9 +91,45 @@ min = w[i]; myfile3 <<  i+1 << " " << w[i] << "\n";
   }
    myfile3.close();
 
-selection(w, 0, 99);
-  for(int i=0;i<100;i++)
+selection(w, 0, 79); // rusiuoju tik pirmus 80
+
+cout<<endl<<"******************************************"<<endl;
+cout<<"Surusiuota :"<<endl<<endl;
+
+  for(int i=0;i<80;i++)
       cout << i+1 << " : " << w[i] << " taskai : " << xx[i][0] << " " <<  xx[i][1] << endl;
+      
+   cout << endl << "Minimumas : " << min << endl;
+   cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;      
+      
+cout<<"******************************************"<<endl<<endl; // nauja sritis
+cout<<"5 geriausi taskai:"<<endl;
+for(int i=0;i<5;i++)
+cout << i+1 << " : " << w[i] << " taskai : " << xx[i][0] << " " <<  xx[i][1] << endl;
+
+  for(int i=0;i<5;i++)
+  {
+            for(int j=0;j<4;j++)
+            {
+x[0] = frand(xx[i][0]-((a2-a1)*0.05), xx[i][0]+((a4-a3)*0.05));
+yy[i*4+j][0] = x[0];
+x[1] = frand(xx[i][1]-((a2-a1)*0.05), xx[i][1]+((a4-a3)*0.05));
+yy[i*4+j][1] = x[1];
+//cout<<yy[i*5+j][0] << "  " << yy[i*5+j][1] << endl;   (tikrinimas)
+  f = branin(&x[0]); 
+  
+  // cout << i*4+j+1 << " : " << f << " taskai : " << x[0] << " " <<  x[1] << endl;
+
+   if (min > f) {
+           min = f; xMin[0] = x[0]; xMin[1] = x[1];
+           cout << endl << "Pagerinome : " << f << " taskai : " << x[0] << " " <<  x[1] << endl; 
+           }; 
+   w[80+i*4+j]=f;
+}  
+}
+
+   cout << endl << "Minimumas : " << min << endl;
+   cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;
 
     system("PAUSE");
     return EXIT_SUCCESS;
