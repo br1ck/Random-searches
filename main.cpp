01a22af90ff677fe70522307ee6f5a5f686eeeff
Robertas <rugius@gmail.com>
2013-11-21 12:36:14 +0200

    ivesta paieska kol nerasime reiksmes su nurodyta paklaida, ivestas dinaminis atminties valdymas

--- main.cpp
+++ main.cpp
@@ -46,20 +46,26 @@ double branin(double *x)
 
 int main(int argc, char *argv[])
 {
-   int ntrial = 10000;
-    double nproc = 0.75;
-    int nclusters = 100;
+   int ntrial = 100;
+    double nproc = 0.85;
+    int nclusters = 5;
+   float eps = 0.5;
+   float gmin = 0.397887;
     
     if (argc > 1) {
     ntrial = atoi(argv[1]);
     nproc = atof(argv[2]);
-    nclusters = atoi(argv[3]); 
+    nclusters = atoi(argv[3]);
+    eps = atof(argv[4]);
+    gmin = atof(argv[5]);
                   }  
     
     double x[9],xMin[9],w[10000];
-    double f,f2,min=0;
+    double f,f2,min=10000;
+    int i=0, size = 100;
     int v1, v2;
-   srand(104);   //srand (time(NULL));
+    double *ww;
+   srand(108);   //srand (time(NULL));
    ofstream myfile, myfile2, myfile3;
    myfile.open ("rezvisixf.txt");
 double a1 = -5 ,a2 = 10 ,a3 = 0 ,a4 = 15;
@@ -140,6 +146,48 @@ yy[i*4+j][1] = x[1];
 if (q == 0) cout << endl << "Nepagerinome :( " <<endl; 
    cout << endl << "Minimumas : " << min << endl;
    cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;
+   
+cout<<"******************************************"<<endl<<endl; // while variantas
+cout<<"Iveskite betka, kad pradeti ieskoti, kol nerasime minimumo " << eps << " tikslumu."<<endl<<endl; // while variantas
+char qqq;
+cin>>qqq;
+min=100000;
+   srand(108);   //srand (time(NULL));   
+ 
+    double *wq = new double [100];   
+   
+while (abs(min-gmin) > eps)
+{
+  x[0] = frand(a1, a2);
+//xx[i][0] = x[0];
+  x[1] = frand(a3, a4);
+//xx[i][1] = x[1];
+  f = branin(&x[0]);
+
+
+    cout << i+1 << " : " << f << " taskai : " << x[0] << " " <<  x[1] << endl;
+ if (min > f) {min = f; xMin[0] = x[0]; xMin[1] = x[1]; };
+ 
+ if (i >= size ) 
+{
+
+ww=wq;    
+wq = new double [size*2];
+size*=2;
+
+for (int j = 0; j < i; j++) wq[j]=ww[j];   
+ delete[] ww;   
+}
+
+
+  wq[i]=f;
+i++; 
+  }
+
+
+   cout << endl << "Minimumas : " << min << endl;
+   cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;
+   cout << endl << "Minimumas su " << eps << " paklaida, rastas per " << i << " bandymu" << endl;   
 
     system("PAUSE");
     return EXIT_SUCCESS;
